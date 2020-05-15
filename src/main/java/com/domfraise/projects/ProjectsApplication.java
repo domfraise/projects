2910de1ad98dcceadfeff993d2a843f7d4580831
FraiseD <dominic.fraise@thehutgroup.com>
2020-05-15 15:52:50 +0100

    Initial Commit

---
+++ src/main/java/com/domfraise/projects/ProjectsApplication.java
@@ -0,0 +1,14 @@
+package com.domfraise.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class ProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsApplication.class, args);
+	}
+
+}
